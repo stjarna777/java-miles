2629c55c180bb27dac1c035de238b0ad51f31b79
anna_stjarna <artemyeva@gmail.com>
2023-01-22 20:25:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int mileCount = 20;
+        int bonusMiles = ticketPrice / mileCount;
+
+        System.out.println("Количество бонусных миль:" + bonusMiles);
+
+    }
+}
